f4fa5d6fce081e8c83dd5181f3a78fade7056e16
Seth Paul <seth@jane.com>
2018-02-02 15:44:52 -0700

    sethp2

--- NameScore/seth_p/name_score.R
+++ NameScore/seth_p/name_score.R
@@ -4,18 +4,20 @@ names <- read_csv('~/Downloads/names.txt')
 names <- names %>% colnames()
 names_df <- data_frame(name=names)
 names_df2 <- names_df %>% 
+  arrange(name) %>%
   mutate(position =row_number())
 
-letters_df  <- data_frame(LETTERS)
+
+letters_df  <- data_frame(LETTERS) 
 letters_df <- letters_df %>% 
-  mutate(position =row_number(),
+  mutate(letter_position =row_number(),
          LETTERS= tolower(LETTERS))
 
 names_df2 %>%
   unnest_tokens(token = 'characters', letter, name) %>%
-  left_join(letters_df, by = c('letter'='LETTERS')) %>% 
-  group_by(position.x) %>%
-  summarise(score=sum(position.y)) %>%
-  mutate(name_score=score*position.x) %>%
+  left_join(letters_df, by = c('letter'='LETTERS')) %>% #head()
+  group_by(position) %>%
+  summarise(score=sum(letter_position)) %>%
+  mutate(name_score=score*position) %>%
   group_by() %>%
   summarise(final_score= sum(name_score, na.rm = T))
