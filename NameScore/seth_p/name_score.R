a28a3df634408fdcdc54a82922cc1e8e47e0c048
Seth Paul <seth@jane.com>
2018-02-02 16:28:13 -0700

    sethp3

--- NameScore/seth_p/name_score.R
+++ NameScore/seth_p/name_score.R
@@ -1,23 +1,26 @@
 library(readr)
 library(tidytext)
-names <- read_csv('~/Downloads/names.txt')
-names <- names %>% colnames()
-names_df <- data_frame(name=names)
+names <- (read_lines('~/Downloads/names.txt') %>% strsplit(split = ','))[[1]]
+names_df <- data_frame(name=names3)
 names_df2 <- names_df %>% 
   arrange(name) %>%
-  mutate(position =row_number())
-
+  mutate(position = row_number())
 
 letters_df  <- data_frame(LETTERS) 
 letters_df <- letters_df %>% 
   mutate(letter_position =row_number(),
          LETTERS= tolower(LETTERS))
 
-names_df2 %>%
+int_names <- names_df2 %>%
+  mutate(name2=name) %>%
   unnest_tokens(token = 'characters', letter, name) %>%
-  left_join(letters_df, by = c('letter'='LETTERS')) %>% #head()
-  group_by(position) %>%
+  left_join(letters_df, by = c('letter'='LETTERS')) %>%
+  group_by(name2, position) %>%
   summarise(score=sum(letter_position)) %>%
-  mutate(name_score=score*position) %>%
+  mutate(name_score=score*position) 
+
+
+int_names %>%
   group_by() %>%
   summarise(final_score= sum(name_score, na.rm = T))
+
