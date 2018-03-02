60a8fee246e9faa132013a374a2eff54a9e0a872
zacanger <zac@zacanger.com>
2018-03-02 15:35:06 -0700

    fix line lengths

--- Chuck/zac/run.sh
+++ Chuck/zac/run.sh
@@ -3,4 +3,6 @@
 if hash xdg-open 2>/dev/null; then opener=xdg-open; else opener=open; fi
 curl https://picsum.photos/800/500 > /tmp/tmp.jpg
 text=`curl -g 'http://api.icndb.com/jokes/random?limitTo=[nerdy]' | jq .value.joke`
-convert -pointsize 12 -fill white -draw "text 20,100 $text" /tmp/tmp.jpg output.jpg && rm /tmp/tmp.jpg && $opener output.jpg
+convert -pointsize 12 -fill white -draw "text 20,100 $text" /tmp/tmp.jpg output.jpg
+rm /tmp/tmp.jpg
+$opener output.jpg
