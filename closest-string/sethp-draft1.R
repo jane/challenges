70d67832d9699b031cd13a265be65f8b3a533d8a
Seth Paul <seth@jane.com>
2018-03-16 15:51:17 -0600

    sethp submission - update for user input

--- closest-string/sethp-draft1.R
+++ closest-string/sethp-draft1.R
@@ -8,38 +8,38 @@ CCTCCCCACTC")
 
 challenge_sequence1 <- c("AACACCCTATA
 CTTCATCCACA
-                        TTTCAATTTTC
-                        ACAATCAAACC
-                        ATTCTACAACT
-                        ATTCCTTATTC
-                        ACTTCTCTATT
-                        TAAAACTCACC
-                        CTTTTCCCACC
-                        ACCTTTTCTCA
-                        TACCACTACTT")
+TTTCAATTTTC
+ACAATCAAACC
+ATTCTACAACT
+ATTCCTTATTC
+ACTTCTCTATT
+TAAAACTCACC
+CTTTTCCCACC
+ACCTTTTCTCA
+TACCACTACTT")
 
 challenge_sequence2<-c("ACAAAATCCTATCAAAAACTACCATACCAAT
 ACTATACTTCTAATATCATTCATTACACTTT
-                       TTAACTCCCATTATATATTATTAATTTACCC
-                       CCAACATACTAAACTTATTTTTTAACTACCA
-                       TTCTAAACATTACTCCTACACCTACATACCT
-                       ATCATCAATTACCTAATAATTCCCAATTTAT
-                       TCCCTAATCATACCATTTTACACTCAAAAAC
-                       AATTCAAACTTTACACACCCCTCTCATCATC
-                       CTCCATCTTATCATATAATAAACCAAATTTA
-                       AAAAATCCATCATTTTTTAATTCCATTCCTT
-                       CCACTCCAAACACAAAATTATTACAATAACA
-                       ATATTTACTCACACAAACAATTACCATCACA
-                       TTCAAATACAAATCTCAAAATCACCTTATTT
-                       TCCTTTAACAACTTCCCTTATCTATCTATTC
-                       CATCCATCCCAAAACTCTCACACATAACAAC
-                       ATTACTTATACAAAATAACTACTCCCCAATA
-                       TATATTTTAACCACTTACCAAAATCTCTACT
-                       TCTTTTATATCCATAAATCCAACAACTCCTA
-                       CTCTCAAACATATATTTCTATAACTCTTATC
-                       ACAAATAATAAAACATCCATTTCATTCATAA
-                       CACCACCAAACCTTATAATCCCCAACCACAC
-                       ")
+TTAACTCCCATTATATATTATTAATTTACCC
+CCAACATACTAAACTTATTTTTTAACTACCA
+TTCTAAACATTACTCCTACACCTACATACCT
+ATCATCAATTACCTAATAATTCCCAATTTAT
+TCCCTAATCATACCATTTTACACTCAAAAAC
+AATTCAAACTTTACACACCCCTCTCATCATC
+CTCCATCTTATCATATAATAAACCAAATTTA
+AAAAATCCATCATTTTTTAATTCCATTCCTT
+CCACTCCAAACACAAAATTATTACAATAACA
+ATATTTACTCACACAAACAATTACCATCACA
+TTCAAATACAAATCTCAAAATCACCTTATTT
+TCCTTTAACAACTTCCCTTATCTATCTATTC
+CATCCATCCCAAAACTCTCACACATAACAAC
+ATTACTTATACAAAATAACTACTCCCCAATA
+TATATTTTAACCACTTACCAAAATCTCTACT
+TCTTTTATATCCATAAATCCAACAACTCCTA
+CTCTCAAACATATATTTCTATAACTCTTATC
+ACAAATAATAAAACATCCATTTCATTCATAA
+CACCACCAAACCTTATAATCCCCAACCACAC
+")
 
 
 
@@ -65,8 +65,25 @@ return(sequences[index,] %>% .[['sequences']])
 
 }
 
-center_hamming(example_seqs)
+example_result <- center_hamming(example_seqs)
+assertthat::are_equal(example_result, 'AATATCTACAT')
+challenge1_result <- center_hamming(challenge_sequence1)
+assertthat::are_equal(challenge1_result, 'ATTCTACAACT')
+challenge2_result <-center_hamming(challenge_sequence2)
+assertthat::are_equal(challenge2_result, 'TTAACTCCCATTATATATTATTAATTTACCC')
 
-center_hamming(challenge_sequence1)
-center_hamming(challenge_sequence2)
 
+user_input_sequences <- function(){
+n_lines <- readline(prompt="Enter number of entries: ")
+for (row_index in 1:n_lines){
+  if (row_index==1){
+    lines<-readline(prompt="")
+  } else {
+  line <- readline(prompt="")
+  lines<- paste(lines,line, sep = '\n')
+  }
+}
+example_result <- center_hamming(lines)
+print(example_result)
+}
+user_input_sequences()
