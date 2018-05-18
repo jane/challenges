19b47750aae9f2c5bb320aa5de2515edfa6391aa
Rcraig <wilcraig@gmail.com>
2018-05-18 16:07:00 -0600

    Working + unworking 1st run golf

---
+++ tally-program/ryanalsowil/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace TallyProgram
+{
+	class Program
+	{
+		static void Main()
+		{
+			var i = "EbAAdbBEaBaaBBdAccbeebaec";
+			var f = i.ToLower().ToCharArray().Distinct().ToDictionary(x => x.ToString(), x => 0);
+			foreach (var c in i)
+			{
+				var x = c.ToString();
+				var u = x.ToUpper();
+				if (u != x)
+					f[x]++;
+				else
+					f[x.ToLower()]--;
+			}
+
+			Console.WriteLine(string.Join(", ", f.OrderBy(d => d.Value).Select(s => $"{s.Key}:{s.Value}")));
+			var z = Console.ReadLine();
+		}
+	}
+}
