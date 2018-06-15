9cbe9fc3486eafc1556de40b0bfe9b7e30bd9e2e
zacanger <zac@zacanger.com>
2018-06-15 15:32:01 -0600

    Whoops, no mod allowed

--- adding-calculator/zac/index.js
+++ adding-calculator/zac/index.js
@@ -5,7 +5,7 @@ console.log((() => {
     const s = process.argv[2]
     const answer = eval(s.replace(/\^/g, '**'))
     if (answer === Infinity) return "Not-defined"
-    if (answer % 1 === 0) return answer
+    if (answer === parseInt(answer)) return answer
     else return "Non-integral answer"
   } catch (_) { }
 })())
